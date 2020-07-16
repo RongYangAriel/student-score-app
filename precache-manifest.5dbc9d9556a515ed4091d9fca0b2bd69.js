cb0c6f3e950c4feedeea101e63d47b2b909d4516
RongY <45574293+RongYangAriel@users.noreply.github.com>
2020-07-16 11:51:58 -0400

    Updates

---
+++ precache-manifest.5dbc9d9556a515ed4091d9fca0b2bd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb574210c651d788aaa9b8ae78a0c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9116d6b7af882b2b504",
+    "url": "/static/css/main.24ef63fb.chunk.css"
+  },
+  {
+    "revision": "e986a84e8409e8843cae",
+    "url": "/static/js/2.9b71710d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b71710d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9116d6b7af882b2b504",
+    "url": "/static/js/main.909faabe.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
